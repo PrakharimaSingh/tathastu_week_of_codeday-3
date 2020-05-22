9dbe4be846ea619bfe5cc87494ed904ed1c2ebac
prakharima singh <65663329+PrakharimaSingh@users.noreply.github.com>
2020-05-22 10:44:17 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,13 @@
+def remove_duplicate(str)
+  s=set(str)
+  s=" ".join(s)
+  print("without order",s)
+  t=" "
+  for i in str:
+    if i in t:
+      pass
+    else:
+      t=t+i
+    print("with order",t)
+str="abbcdefaade"
+remove_duplicate(str)
